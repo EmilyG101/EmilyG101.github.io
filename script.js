bf72e7c603336e25d5ad73e28854ff2676851ad7
EmilyG101 <101006556+EmilyG101@users.noreply.github.com>
2024-05-24 09:55:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-window.alert ("Welcome to my blogpost!");
+//window.alert ("Welcome to my blogpost!");
 //text size change
-document.querySelector('select').onchange= function(){
-  document.querySelector('p').style.fontSize =this.value;
-}
+//document.querySelector('select').onchange= function(){
+  //document.querySelector('p').style.fontSize =this.value;
+//}
 
-function greet()
-{
-  let name =
- document.getElementById("name").value;
-  if (name === ''){
-name = 'world'
-}
-document.getElementById('result').innerHTML= 'Thanks for coming to read, '+ name;
-}
+//function greet()
+//{
+  //let name =
+// document.getElementById("name").value;
+  //if (name === ''){
+//name = 'world'
+//}
+//document.getElementById('result').innerHTML= 'Thanks for coming to read, '+ name;
+//}
 
