5fb2b41d469e13efdcde62f705b67142d088c4b8
EmilyG101 <101006556+EmilyG101@users.noreply.github.com>
2024-05-24 09:49:30 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+window.alert ("Welcome to my blogpost!");
+//text size change
+document.querySelector('select').onchange= function(){
+  document.querySelector('p').style.fontSize =this.value;
+}
+
+function greet()
+{
+  let name =
+ document.getElementById("name").value;
+  if (name === ''){
+name = 'world'
+}
+document.getElementById('result').innerHTML= 'Thanks for coming to read, '+ name;
+}
+
